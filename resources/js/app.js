8c356bd8884ebafee5f4ab827ce58dd754b926fc
isnaenisetiyadi <isnaeni_s2te12@mail.ugm.ac.id>
2020-04-20 12:32:39 +0800

    Berhasil memengkompile kode js vue router

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,25 +8,36 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
 
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
+
+const routes = [
+    { path: '/foo', component: Foo },
+    { path: '/bar', component: Bar }
+  ]
+
+  const router = new VueRouter({
+    routes // short for `routes: routes`
+  })
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+// const app = new Vue({
+//     el: '#app',
+// });
+
 const app = new Vue({
-    el: '#app',
-});
+    router
+  }).$mount('#app')
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,9 +5,10 @@
 <div class="jumbotron" id="jumbotron_index">
   <h1 class="display-4">Hello, world!</h1>
   <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  <router-link to="/foo">Go to Foo</router-link>
+    <router-link to="/bar">Go to Bar</router-link>
+
+    <router-view></router-view>
 </div>
 </div>
 @endsection
